c3e44a0e5ca198c1ffda5b388a2836e3f38ffc12
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-01-15 20:09:29 +0200

    commit

---
+++ js/modal.js
@@ -0,0 +1,28 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+  // <script>
+  //     (() => {
+  //       document
+  //           .quarySelector('.js-speaker-form')
+  //           .addEventListener('submit', e => {
+  //             e.preventDefault();
+
+  //             new FormData(e.currentTarget).forEach((value, name) =>
+  //               console.log(`${name}: ${value}`),
+  //             );
+  //           });
+  //     })();
+  // </script> 
